8650844dfb723f01d8bf8d318ba993fa969b4076
kgarsjo <kgarsjo@kgarsjo-VirtualBox.(none)>
2013-01-23 14:06:20 -0800

    Starting uoenc arg checks

--- uoenc.c
+++ uoenc.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+
+/* -------------------------------- //
+	Golbal Variables
+// -------------------------------- */
+const char* const dFlagStr= "-d";
+const char* const lFlagStr= "-l";
+
 /* -------------------------------- //
 	Function Prototypes
 // -------------------------------- */
@@ -7,17 +15,36 @@ void printUsage();
 	Program Entry
 // -------------------------------- */
 int main(int argc, char **argv) {
-	if (argc == 1) {
+	char *inputFile		= NULL;
+	char *outputIP		= NULL;
+	char *outputPort	= NULL;
+	
+	int dumpFlag		= 0;
+	int localFlag		= 0;
+	int validArgs		= 0;
 
-	} else if (argc == 2) {
+	// Argument validity checks
+	if (argc >= 2) {
+		inputFile= argv[1];
+		if (argc == 3) {
+			
+		} else if (argc == 4) {
 
-	} else if (argc == 4) {
+		} else if (argc == 5) {
 
-	} else {
+		}
+	}
+
+	if (!validArgs) {
 		printUsage();
 		return 1;
 	}
 	
-	printf("%s", "All is well!");
+	printf("%s\n", "All is well!");
 	return 0;
+}
+
+
+void printUsage() {
+	printf("%s\n", "Usage: uoenc <input file> [-d <output IP-addr:port>] [-l]");
 }
