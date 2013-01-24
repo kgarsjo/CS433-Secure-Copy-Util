cfce8ccf70e7ea9139ff2f3fe0e505f6fb367f37
kgarsjo <kgarsjo@kgarsjo-VirtualBox.(none)>
2013-01-23 17:34:59 -0800

    Starting password input in uoenc

--- uoenc.c
+++ uoenc.c
@@ -7,27 +7,36 @@
 // -------------------------------- */
 const char* const dFlagStr= "-d";
 const char* const lFlagStr= "-l";
+const size_t blocksize= 1024;
 
 /* -------------------------------- //
 	Function Prototypes
 // -------------------------------- */
+void encrypt();
 void gcryptInit();
 void printUsage();
+void save();
+void transmit();
 
 /* -------------------------------- //
 	Program Entry
 // -------------------------------- */
 int main(int argc, char **argv) {
-	char *inputFile		= NULL;
-	char *outputIP		= NULL;
-	char *outputPort	= NULL;
-	
+	char *inputFile		= (char*) malloc(sizeof(char) * blocksize);
+	char *outputIP		= (char*) malloc(sizeof(char) * blocksize);
+	char *outputPort	= (char*) malloc(sizeof(char) * blocksize);
+	char *password		= NULL;	
+
 	int dumpFlag		= 0;
 	int localFlag		= 0;
 	int validArgs		= 0;
 
+	gcry_cipher_hd_t handle;
+	gcry_error_t	 error;
+
+
 	gcryptInit();
-	
+
 	// Argument validity checks
 	if (argc >= 2) {
 		inputFile= argv[1];
@@ -41,11 +50,11 @@ int main(int argc, char **argv) {
 			if (resd == 0) {
 				if (dumpFlag) { validArgs= 0; break; }
 				dumpFlag= 1;
-				
+
 				i++;
 				if (i >= argc) { validArgs= 0; break; }
 				outputIP= argv[i];
-				
+
 				validArgs= 1;
 
 			// Local flag detection
@@ -64,10 +73,27 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	// TODO Actual work begins here	
-	printf("%s\n", "All is well!");
+	// Prompt for password and encrypt
+	printf("Password: ");
+	size_t size= blocksize;
+	int res= getline(&password, &size, stdin);
+	if (res == -1) {
+		printf("\nERROR\n");
+	}
+
+	encrypt(inputFile);
+	if (localFlag) {
+		save();
+	}
+	if (dumpFlag) {
+		transmit();
+	}
+	
 	return 0;
 }
+
+
+void encrypt() { };
 
 
 void gcryptInit() {
@@ -86,3 +112,9 @@ void gcryptInit() {
 void printUsage() {
 	printf("%s\n", "Usage: uoenc <input file> [-d <output IP-addr:port>] [-l]");
 }
+
+
+void save() { }
+
+
+void transmit() { }
