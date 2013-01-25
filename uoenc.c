745a1c7c74aabea563af604675a390115e692cec
Kevin Garsjo <kgarsjo@uoregon.edu>
2013-01-24 16:36:52 -0800

    Filesystem harness in place; nothing operable yet

--- uoenc.c
+++ uoenc.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <gcrypt.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,11 +13,11 @@ const size_t blocksize= 1024;
 /* -------------------------------- //
 	Function Prototypes
 // -------------------------------- */
-void encrypt();
+void encrypt(char*, char*);
 void gcryptInit();
 void printUsage();
-void save();
-void transmit();
+void save(char*);
+void transmit(char*, char*);
 
 /* -------------------------------- //
 	Program Entry
@@ -31,43 +32,43 @@ int main(int argc, char **argv) {
 	int localFlag		= 0;
 	int validArgs		= 0;
 
-	gcry_cipher_hd_t handle;
-	gcry_error_t	 error;
 
 
 	gcryptInit();
 
 	// Argument validity checks
 	if (argc >= 2) {
-		inputFile= argv[1];
+		strcpy(argv[1], inputFile); //TODO Error checking
+		validArgs= 1;
+		
+		if (argc > 2) {
+			int i;
+			for (i= 2; i < argc; i++) {
+				int resd= strcmp(dFlagStr, argv[i]);
+				int resl= strcmp(lFlagStr, argv[i]);
 
-		int i;
-		for (i= 2; i < argc; i++) {
-			int resd= strcmp(dFlagStr, argv[i]);
-			int resl= strcmp(lFlagStr, argv[i]);
+				// Dump flag detection
+				if (resd == 0) {
+					if (dumpFlag) { validArgs= 0; break; }
+					dumpFlag= 1;
 
-			// Dump flag detection
-			if (resd == 0) {
-				if (dumpFlag) { validArgs= 0; break; }
-				dumpFlag= 1;
+					i++;
+					if (i >= argc) { validArgs= 0; break; }
+					outputIP= argv[i];
 
-				i++;
-				if (i >= argc) { validArgs= 0; break; }
-				outputIP= argv[i];
+					validArgs= 1;
 
-				validArgs= 1;
+				// Local flag detection
+				} else if (resl == 0) {
+					if (localFlag) { validArgs= 0 ; break; }
+					localFlag= 1;
+					validArgs= 1;
 
-			// Local flag detection
-			} else if (resl == 0) {
-				if (localFlag) { validArgs= 0 ; break; }
-				localFlag= 1;
-				validArgs= 1;
-
-			// Catch-all for everything else
-			} else { validArgs= 0; break; }
+				// Catch-all for everything else
+				} else { validArgs= 0; break; }
+			}
 		}
 	}
-
 	if (!validArgs) {
 		printUsage();
 		return 1;
@@ -78,22 +79,81 @@ int main(int argc, char **argv) {
 	size_t size= blocksize;
 	int res= getline(&password, &size, stdin);
 	if (res == -1) {
-		printf("\nERROR\n");
+		perror("getline:");
+		exit(3);
 	}
 
-	encrypt(inputFile);
+	encrypt(inputFile, password);
 	if (localFlag) {
-		save();
+		save(NULL);
 	}
 	if (dumpFlag) {
-		transmit();
+		transmit(NULL, NULL);
 	}
 	
 	return 0;
 }
 
 
-void encrypt() { };
+void encrypt(char *filename, char *key) { 
+	gcry_cipher_hd_t handle;
+	gcry_error_t	 error;
+	
+	// Set the handle and key for AES256 algo
+	error= gcry_cipher_open(&handle, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_CBC, GCRY_CIPHER_SECURE);
+	if (error) {
+		printf("Error in opening handler!\n");
+	}
+
+	error= gcry_cipher_setkey(handle, key, sizeof(key));
+	if (error) {
+		printf("Error in keyset!\n");
+	}
+
+
+	// Buffer the plaintext
+	FILE *fptr= fopen(filename, "r");
+	if (fptr == NULL) {
+		perror("Error opening plaintext: ");
+	}
+
+	int res= fseek(fptr, 0, SEEK_END);
+	if (res == -1) {
+		perror("Error reading file length: ");
+	}
+
+	size_t fsize= ftell(fptr);
+	res= fseek(fptr, 0, SEEK_SET);
+
+	char *plaintext= (char*) malloc(sizeof(char) * fsize);
+	size_t numread= fread(plaintext, fsize, 1, fptr);
+	//TODO Error handling
+
+	fclose(fptr);
+
+
+	// Encrypt the buffer
+	char *ciphertext= (char*) malloc(sizeof(char) * fsize * 100);	
+	error= gcry_cipher_encrypt(handle, ciphertext, sizeof(ciphertext), plaintext, sizeof(plaintext));
+	if (error) {
+		printf("Error encrypting the plaintext!\n");
+	}
+
+
+	// Output the ciphertext
+	// TODO Change method return to a ptr to ciphertext block and handle ciphertext elsewhere
+	char *ciphername= (char*) malloc(sizeof(filename) + 3);
+	strcpy(ciphername, filename);
+	strcat(ciphername, ".uo"); //TODO make extension global constant
+	
+	FILE* cptr= fopen(ciphername, "W+");
+	if (cptr == NULL) {
+		perror("Error creating ciphertext file: ");
+	}
+
+	fwrite(ciphertext, sizeof(ciphertext), 1, cptr);
+	fclose(cptr); 
+}
 
 
 void gcryptInit() {
@@ -114,7 +174,11 @@ void printUsage() {
 }
 
 
-void save() { }
+void save(char* filename) { 
+
+}
 
 
-void transmit() { }
+void transmit(char* ipaddr, char* port) { 
+
+}
